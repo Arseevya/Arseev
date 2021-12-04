be5b7550cfe4bb2ae79e883bccae70fb04e511d4
Arseevya <93937555+Arseevya@users.noreply.github.com>
2021-12-04 14:21:54 +0500

    Дз за 10 урок (29.11.21) в работе. 14:20 04.12.2021

---
+++ На уроке/Работа с php/10 урок/10  урок.php
@@ -0,0 +1,27 @@
+<?php
+/*$a_arr=[];
+for ($i=0;$i < 2; $i++){
+    for ($m=0; $m<3;$m++){
+        $a_arr[$i][$m]=mt_rand(0,55);
+    }
+}
+$b_arr=[];
+foreach ($a_arr as $_sub) {
+    foreach ($_sub as $el){
+        if ($el % 2==0){
+            $b_arr[]=$el;
+        }
+    }
+}
+print_r($a_arr);
+echo "<br>";
+print_r($b_arr)
+*/?>
+<?php
+$str="FACT";
+$arr1=str_split($str, 3);
+print_r($arr1);
+$arr2[0]=substr($str, 0, 1);
+$arr2[1]=substr($str,1 , 3);
+print_r($arr2);
+?>
